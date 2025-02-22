67b3cb816536bdd2b25aee4c393cd23608531e9b
UnoSite <68971788+UnoSite@users.noreply.github.com>
2025-02-22 23:52:38 +0100

    Update const.py

--- custom_components/isitpayday/const.py
+++ custom_components/isitpayday/const.py
@@ -5,6 +5,6 @@ DOMAIN = "isitpayday"
 CONF_COUNTRY = "country"
 CONF_COUNTRY_ID = "country_id"
 
-# Hent version automatisk fra manifest.json
+# Get version automatically from manifest.json
 with open(Path(__file__).parent / "manifest.json", encoding="utf-8") as manifest_file:
     VERSION = json.load(manifest_file)["version"]

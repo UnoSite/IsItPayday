667b3042d83384966a34e19bb6f843654ad0f0b6
UnoSite <68971788+UnoSite@users.noreply.github.com>
2025-02-22 16:09:04 +0100

    Update __init__.py

--- custom_components/isitpayday/__init__.py
+++ custom_components/isitpayday/__init__.py
@@ -1,19 +1,27 @@
 """Is It Payday? integration."""
+import logging
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers.aiohttp_client import async_get_clientsession
 from .const import DOMAIN
+
+_LOGGER = logging.getLogger(__name__)
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up Is It Payday? from a config entry."""
     hass.data.setdefault(DOMAIN, {})
     hass.data[DOMAIN][entry.entry_id] = entry
 
-    hass.async_create_task(
-        hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
-    )
+    session = async_get_clientsession(hass)
+    hass.data[DOMAIN][f"{entry.entry_id}_session"] = session
+
+    hass.config_entries.async_setup_platforms(entry, ["sensor"])
 
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload Is It Payday? config entry."""
+    if f"{entry.entry_id}_session" in hass.data[DOMAIN]:
+        del hass.data[DOMAIN][f"{entry.entry_id}_session"]
+
     return await hass.config_entries.async_unload_platforms(entry, ["sensor"])

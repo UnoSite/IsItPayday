f43033c809b148f72c581eddf457af54fa1a691f
UnoSite <68971788+UnoSite@users.noreply.github.com>
2025-02-23 22:32:10 +0100

    Update const.py

--- custom_components/isitpayday/const.py
+++ custom_components/isitpayday/const.py
@@ -4,6 +4,8 @@ from pathlib import Path
 DOMAIN = "isitpayday"
 CONF_COUNTRY = "country"
 CONF_COUNTRY_ID = "country_id"
+CONF_PAYDAY_TYPE = "payday_type"
+CONF_CUSTOM_DAY = "custom_day"
 
 # Get version automatically from manifest.json
 with open(Path(__file__).parent / "manifest.json", encoding="utf-8") as manifest_file:
